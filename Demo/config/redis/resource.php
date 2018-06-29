daa420756e05222c41f2f8201f2e5e12d9331f79
zhiyuan12 <hzyhouzhiyuan@qq.com>
2018-06-29 14:50:23 +0800

    delete demo

--- Demo/config/redis/resource.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- *     这里主要存放redis资源
- */
-return array(
-    'business'      => array('host' => 'xxx', 'port' => 11565, 'timeout' => 2.5),
-    'business_read' => array('host' => 'xxx', 'port' => 11565, 'timeout' => 2.5)
-
-);

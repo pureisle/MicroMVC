261a120c3641e77ddbcc4826392f8d1d62a7dec4
侯志远 <zhiyuan12@staff.weibo.com>
2018-08-16 17:20:59 +0800

    增加开发环境redis配置

---
+++ Framework/config/dev/api_redis.php
@@ -0,0 +1,5 @@
+<?php
+return array(
+    'platform_token'      => array('host' => '127.0.0.1', 'port' => 6379, 'timeout' => 2.5),
+    'platform_token_read' => array('host' => '127.0.0.1', 'port' => 6379, 'timeout' => 2.5)
+);

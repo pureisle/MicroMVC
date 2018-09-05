f04bcae475638bbc19602190c3cc233efbd02e64
侯志远 <zhiyuan12@staff.weibo.com>
2018-09-05 15:15:43 +0800

    增加根据类名查询文件方法

--- Framework/Models/AutoLoad.php
+++ Framework/Models/AutoLoad.php
@@ -31,22 +31,34 @@ class AutoLoad {
      */
     public function loadClass($class_name) {
         $class_name = ltrim($class_name, '\\');
-        if ($last_ns_pos = strrpos($class_name, '\\')) {
-            $namespace = substr($class_name, 0, $last_ns_pos);
-            $tmp_ns    = explode('\\', $namespace);
-            //只加在框架类或者指定app的类库,隔离app间model相互调用
-            if (FRAMEWORK_NAME != $tmp_ns[0] && ! empty($this->_load_path_prefix) && $tmp_ns[0] != $this->_load_path_prefix) {
-                return false;
-            }
-            $class_name = substr($class_name, $last_ns_pos + 1);
-            $file_name  = '';
-            if ( ! empty($namespace)) {
-                $file_name = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-            }
-            $file_name .= str_replace('_', DIRECTORY_SEPARATOR, $class_name) . '.php';
-            require_once ROOT_PATH . DIRECTORY_SEPARATOR . $file_name;
-            return true;
+        $tmp_ns     = explode('\\', $class_name);
+        //只加在框架类或者指定app的类库,隔离app间model相互调用
+        if (FRAMEWORK_NAME != $tmp_ns[0] && ! empty($this->_load_path_prefix) && $tmp_ns[0] != $this->_load_path_prefix) {
+            return false;
         }
-        return false;
+        $file_path = self::getFilePath($class_name);
+        require_once $file_path;
+        return true;
+    }
+    /**
+     * 根据类名获取文件路径
+     * @param  string   $class_name
+     * @return string
+     */
+    public static function getFilePath($class_name) {
+        $class_name  = ltrim($class_name, '\\');
+        $last_ns_pos = strrpos($class_name, '\\');
+        if (false === $last_ns_pos) {
+            return false;
+        }
+        $namespace  = substr($class_name, 0, $last_ns_pos);
+        $class_name = substr($class_name, $last_ns_pos + 1);
+        $file_name  = '';
+        if ( ! empty($namespace)) {
+            $file_name = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+        }
+        $file_name .= str_replace('_', DIRECTORY_SEPARATOR, $class_name) . '.php';
+        $path = ROOT_PATH . DIRECTORY_SEPARATOR . $file_name;
+        return $path;
     }
 }

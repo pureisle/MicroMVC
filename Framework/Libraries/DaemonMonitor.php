983e92d3b5339fce32ce50b07bb1959bc3b57237
侯志远 <zhiyuan12@staff.weibo.com>
2019-01-14 17:26:53 +0800

    修改注释

--- Framework/Libraries/DaemonMonitor.php
+++ Framework/Libraries/DaemonMonitor.php
@@ -6,7 +6,8 @@
  * //{Daemon 类名} => array(
  * //    'count' => {启动进程个数}
  * //    'time_out' => {最大执行时间}  //单位 秒，可以为小数
- * //    'log_config_name'  // 日志配置名
+ * //    'log_config_name'=>'',  // 日志配置名
+ * //    'params'=>array()  //进程初始化参数
  * // )
  * return array(
  * 'CountPVUV' => array(

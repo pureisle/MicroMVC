96091288cd8653d2bf9f0ff9072aabe5cba7cc30
侯志远 <zhiyuan12@staff.weibo.com>
2019-03-22 09:50:07 +0800

    完善创建Module

--- public/create_module.php
+++ public/create_module.php
@@ -13,10 +13,42 @@ $bootstrap_str = <<<EOT
  */
 namespace {module_name};
 use Framework\Models\Dispatcher;
-define("{up_module_name}_MODULE_ROOT", ROOT_PATH . "/{module_name}");
-class Bootstrap extends \Framework\Models\Bootstrap {
-	public function _initDemo(Dispatcher \$dispatcher) {}
+use Framework\Models\Bootstrap as FB;
+class Bootstrap extends FB {
+    public function _initDemo(Dispatcher \$dispatcher) {}
 }
+EOT;
+$controller_str = <<<EOT
+<?php
+/**
+ * 控制器
+ */
+namespace {module_name}\Controllers;
+use Framework\Models\Controller;
+
+class Index extends Controller {
+    public static \$INDEX_PARAM_RULES = array(
+        'a'   => '',
+        'b'   => '',
+    );
+    public function indexAction() {
+        // \$this->useCORS();
+        \$this->usePolicy()->useXSS()->disableSniffing()->useFrame()->forceHTTPS();
+        \$params = \$this->getGetParams();
+         \$this->getView()->assign(array("text" => 'hello,MicroMVC.'));
+        //\$this->localtion(\$url);
+    }
+}
+EOT;
+$view_str = <<<EOT
+<html>
+<head>
+<title>micro-mvc</title>
+</head>
+<body>
+    <p><?php echo \$text; ?></p>
+  </body>
+</html>
 EOT;
 require "cli.php";
 function main($config, $argv) {
@@ -29,13 +61,26 @@ function main($config, $argv) {
         die("Error: dir " . $target_file . " exist.\n");
     }
     mkdir($target_file, 0775);
-    $create_dir_set = array('Libraries', 'Tests', 'Controllers', 'Views', 'Plugins', 'Cache', 'Data', 'Entities', 'Models', 'config');
-    foreach ($create_dir_set as $dir_name) {
-        $tmp = $target_file . "/" . $dir_name;
+    $path = $config['path'];
+    foreach ($path as $key => $dir_name) {
+        if ("/" === $dir_name[0]) {
+            $tmp = $dir_name;
+        } else {
+            $tmp = $target_file . "/" . $dir_name;
+        }
+        if (is_dir($tmp)) {
+            continue;
+        }
         $ret = mkdir($tmp, 0775);
+        echo "cteated dir :" . $tmp . "\n";
     }
     global $bootstrap_str;
-    $up_module_name = strtoupper($module_name);
-    file_put_contents($target_file . "/Bootstrap.php", str_replace('{up_module_name}', $up_module_name, str_replace('{module_name}', $module_name, $bootstrap_str)));
+    file_put_contents($target_file . "/Bootstrap.php", str_replace('{module_name}', $module_name, $bootstrap_str));
+    global $controller_str;
+    file_put_contents($target_file . '/' . $path['controller'] . "/Index.php", str_replace('{module_name}', $module_name, $controller_str));
+    global $view_str;
+    $tmp = $target_file . '/' . $path['view'] . "/Index";
+    mkdir($tmp, 0775);
+    file_put_contents($tmp . "/index.phtml", $view_str);
     die("Create success!\n");
 }

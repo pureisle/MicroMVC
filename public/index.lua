a5fa0af76ff84120e31f70e75b1b3591af986096
侯志远 <zhiyuan12@staff.weibo.com>
2019-04-03 15:55:07 +0800

    完善lua mvc

--- public/index.lua
+++ public/index.lua
@@ -13,38 +13,3 @@ require "GlobalFunction" --加载自定义公共库
 Application = require "Application"
 local app = Application:new(REQUEST_URI)
 app:run()
-
--- Redis = require "Redis"
--- red = Redis:new();
--- red:set_timeout(1000)
-
--- for k, v in pairs(ngx.req.get_headers()) do
---     ngx.say(k, ": ", v)
--- end
-
---get请求uri参数
--- ngx.say("uri args begin", "<br/>")
--- local uri_args = ngx.req.get_uri_args()
--- for k, v in pairs(uri_args) do
---     if type(v) == "table" then
---         ngx.say(k, " : ", table.concat(v, ", "), "<br/>")
---     else
---         ngx.say(k, ": ", v, "<br/>")
---     end
--- end
--- ngx.say("uri args end", "<br/>")
--- ngx.say("<br/>")
-
---post请求参数
--- ngx.req.read_body()
--- ngx.say("post args begin", "<br/>")
--- local post_args = ngx.req.get_post_args()
--- for k, v in pairs(post_args) do
---     if type(v) == "table" then
---         ngx.say(k, " : ", table.concat(v, ", "), "<br/>")
---     else
---         ngx.say(k, ": ", v, "<br/>")
---     end
--- end
--- ngx.say("post args end", "<br/>")
--- ngx.say("<br/>")

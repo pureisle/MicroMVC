f9e7d37f4e57f8fc8b26b8f588c4f17134833c03
侯志远 <zhiyuan12@staff.weibo.com>
2019-04-24 15:11:47 +0800

    更新测试样例

--- Sso/Controllers/Api/Index.lua
+++ Sso/Controllers/Api/Index.lua
@@ -1,22 +1,22 @@
 -- 首先继承Controller基类
 local Sso_Controller = Controller:new()
+local sm = require 'Sso/Models/Sample'
 function Sso_Controller:indexAction()
     -- Profiler = require 'Profiler'
     -- Profiler:setGetTimeMethod(function () return microtime(true) end)
     -- Profiler:start()
-
+    
     local Redis = require 'Redis'
-    local connect_time
+    -- var_dump(package.loaded)
     for i = 1, 1 do
-        rt = Redis:new()
-        rt:connect('redis:session', 'Sso')
+        local rt = Redis:new()
+        rt:connect('redis:test', 'Sso')
         local reuse = rt:get_reused_times()
         -- var_dump(rt:set('test_key','test_value'))
-        var_dump(rt:get('test_key'))
+        var_dump(rt:hgetall('Vote\\Data:Vote:Count:2019_172'))
         local times, er = rt:set_keepalive(600, 100)
-        ngx.say(times, '--', reuse)
+        -- ngx.say(times, '--', reuse)
     end
-    local sm = require 'Sso/Models/Sample'
     sm:new()
 
     -- Profiler:stop()
--- Sso/config/lua_config/dev/redis.lua
+++ Sso/config/lua_config/dev/redis.lua
@@ -1,5 +1,5 @@
 return {
     session = {host = '127.0.0.1', port = 6379, timeout = 2.5},
-    session_read = {host = '127.0.0.1', port = 6379, timeout = 2.5}
-
+    session_read = {host = '127.0.0.1', port = 6379, timeout = 2.5},
+    test = {host = '127.0.0.1', port = 51189, timeout = 2.5}
 }

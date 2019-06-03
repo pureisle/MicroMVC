a067825ad9824a3faa9a24d0f322df5f3f8f24c4
xiaodu <hzyhouzhiyuan@qq.com>
2019-06-03 04:18:14 -0400

    变更lua配置文件路径；增加ini配置解析

--- Framework/Luas/ConfigTool.lua
+++ Framework/Luas/ConfigTool.lua
@@ -4,29 +4,43 @@
 --]]
 local string_sub = string.sub
 local table_remove = table.remove
+local ini = require 'IniParser'
+local DIRECTORY_SEPARATOR = FRAMEWORK.DIRECTORY_SEPARATOR
+local ROOT_PATH = FRAMEWORK.ROOT_PATH
+local CONFIG_FOLDER = FRAMEWORK.PATH.config
 local ConfigTool = Class:new('ConfigTool')
 ConfigTool.FILE_SUFFIX = '.lua'
-ConfigTool.CACHE_TIME = 600
--- 构造方法
-function ConfigTool:new ()
-    return self
-end
-function ConfigTool:getConfig(file_name, module_name)
+ConfigTool.INI_SUFFIX = '.ini'
+_ConfigTool_CACHE = {}
+function ConfigTool:getConfig(file_name, module_name, file_suffix)
     if empty(file_name) then
         return false
     end
-    local file_path = self:getFilePath(file_name, module_name, '');
-    return require(string_sub(file_path, 1, -#(self.FILE_SUFFIX) - 1))
-
+    file_suffix = file_suffix or self.FILE_SUFFIX
+    local file_path = self:getFilePath(file_name, module_name, file_suffix);
+    if not empty(_ConfigTool_CACHE[file_path]) then
+        return _ConfigTool_CACHE[file_path]
+    end
+    if not file_exists(file_path) then
+        return false
+    end
+    local config
+    if file_suffix == self.FILE_SUFFIX then
+        config = require(string_sub(file_path, 1, -#(self.FILE_SUFFIX) - 1))
+    elseif file_suffix == self.INI_SUFFIX then
+        config = ini:decodeByFile(file_path, true)
+    else
+        config = file_get_contents(file_path)
+    end
+    _ConfigTool_CACHE[file_path] = config
+    return config
 end
-function ConfigTool:getFilePath(file_name, module_name)
-    local ROOT_PATH = FRAMEWORK.ROOT_PATH
-    local DIRECTORY_SEPARATOR = FRAMEWORK.DIRECTORY_SEPARATOR
-    local CONFIG_FOLDER = FRAMEWORK.CONFIG_FOLDER
-    local path = ROOT_PATH .. DIRECTORY_SEPARATOR .. module_name .. DIRECTORY_SEPARATOR .. CONFIG_FOLDER .. DIRECTORY_SEPARATOR .. file_name .. self.FILE_SUFFIX;
+function ConfigTool:getFilePath(file_name, module_name, file_suffix)
+    file_suffix = file_suffix or self.FILE_SUFFIX
+    local path = ROOT_PATH .. DIRECTORY_SEPARATOR .. module_name .. DIRECTORY_SEPARATOR .. CONFIG_FOLDER .. DIRECTORY_SEPARATOR .. file_name .. file_suffix
     local env = Tools:getEnv();
     if Tools.ENV_PRO ~= env then
-        local test_path = ROOT_PATH .. DIRECTORY_SEPARATOR .. module_name .. DIRECTORY_SEPARATOR .. CONFIG_FOLDER .. DIRECTORY_SEPARATOR .. env .. DIRECTORY_SEPARATOR .. file_name .. self.FILE_SUFFIX
+        local test_path = ROOT_PATH .. DIRECTORY_SEPARATOR .. module_name .. DIRECTORY_SEPARATOR .. CONFIG_FOLDER .. DIRECTORY_SEPARATOR .. env .. DIRECTORY_SEPARATOR .. file_name .. file_suffix
         if file_exists(test_path) then
             path = test_path
         end
@@ -38,9 +52,9 @@ end
 --    如：log.file_name:firehose,将获取 module_name 下config内的log文件夹内的配置文件file_name内的firehose配置项
 --
 --    @param  string  config_name 字符串解析规则：配置文件名_配置名
-function ConfigTool:loadByName(config_name, module_name)
+function ConfigTool:loadByName(config_name, module_name, file_suffix)
     if empty(config_name) then
-        return {}
+        return false
     end
     local tmp = explode('.', config_name)
     local file_name = table_remove(tmp)
@@ -52,11 +66,11 @@ function ConfigTool:loadByName(config_name, module_name)
     end
     local file_path
     if not empty(tmp) then
-        file_path = implode(FRAMEWORK.DIRECTORY_SEPARATOR, tmp) .. FRAMEWORK.DIRECTORY_SEPARATOR . file_name;
+        file_path = implode(DIRECTORY_SEPARATOR, tmp) .. DIRECTORY_SEPARATOR .. file_name;
     else
         file_path = file_name;
     end
-    local config_array = self:getConfig(file_path, module_name)
+    local config_array = self:getConfig(file_path, module_name, file_suffix)
     if (empty(resource_name) or empty(config_array[resource_name])) then
         return config_array
     end
--- Sso/config/redis.lua
+++ Sso/config/redis.lua
@@ -1,5 +1,4 @@
 return {
     session = {host = '127.0.0.1', port = 6379, timeout = 2.5},
     session_read = {host = '127.0.0.1', port = 6379, timeout = 2.5}
-
 }

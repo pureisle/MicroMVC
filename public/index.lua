c2ef51a8be2aade47e0a3ff880ca2a1f74d2ee44
xiaodu <hzyhouzhiyuan@qq.com>
2019-06-03 04:06:06 -0400

    环境变量设置默认值

--- public/index.lua
+++ public/index.lua
@@ -2,7 +2,7 @@
 -- 入口文件
 -- @author zhiyuan <zhiyuan12@staff.weibo.com>
 --]]
-FRAMEWORK.CURRENT_ENV_NAME = ngx.var.CURRENT_ENV_NAME --读取nginx配置的运行环境变量
+FRAMEWORK.CURRENT_ENV_NAME = ngx.var.CURRENT_ENV_NAME or FRAMEWORK.CURRENT_ENV_NAME --读取nginx配置的运行环境变量
 local Application = require("Application")
 local app = Application:new(ngx.var.request_uri)
 app:run()

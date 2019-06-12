213d471f38083e1e504955bcb0af1c6136f45cba
xiaodu <hzyhouzhiyuan@qq.com>
2019-06-12 02:21:53 -0400

    修改index样例

---
+++ Index/Controllers/Index.lua
@@ -0,0 +1,7 @@
+-- 首先继承Controller基类
+local Index_Controller = Controller:new()
+function Index_Controller:indexAction()
+    ngx.say('Hello,MicroMVC')
+    return true
+end
+return Index_Controller
--- Index/Controllers/Index.php
+++ Index/Controllers/Index.php
@@ -6,14 +6,13 @@ use Framework\Models\Controller;
 
 class Index extends Controller {
     public function init() {
-        // echo "init\n";
-        //  safe_exit();//安全退出
-    }
-    public function indexAction() {
         // $lc = new LocalCurl();
         // $lc->setAction('test', 'http://127.0.0.1/xhprof/xhprof/run');
         // var_dump($lc->get('test')->body());
-        echo "hello,MicroMVC~";
-        return false;
+        // echo "init\n";
+        // safe_exit();//安全退出
+    }
+    public function indexAction() {
+        $this->getView()->assign(array("text" => 'Hello,MicroMVC~'));
     }
 }

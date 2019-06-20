fece0965823047f8c0afa997fe820ab2d689d498
xiaodu <hzyhouzhiyuan@qq.com>
2019-06-19 22:32:08 -0400

    增加require工具

--- Framework/Luas/Tools.lua
+++ Framework/Luas/Tools.lua
@@ -12,6 +12,7 @@ Tools.ENV_DEV = 'dev' --开发环境
 Tools.ENV_PRO = 'pro' --生产环境
 Tools.ENV_INDEX_NAME = 'CURRENT_ENV_NAME'
 Tools.env_name = ''
+Tools._REQUIRE_CACHE = {}
 -- 获取代码执行环境
 function Tools:getEnv()
     --生产环境禁止变更设置
@@ -37,15 +38,36 @@ function Tools:isCli()
     end
     return false;
 end
--- 可以根据相对路径加载module包
+-- PHP的require方式加载
+-- 可以按相对路径或绝对路径加载文件，需要指定文件后缀。
+-- Tools:require('../../Framework/Luas/ConfigTool.lua')
 function Tools:require(module_name)
-    local path = dirname(debug_getinfo(2, "S").source:sub(2))
-    local module_path = string_gsub(module_name, '[.]', '/')
-    local real_path = path..'/'..module_path
-    if(not empty(package.loaded[real_path]) or file_exists(real_path..'.lua')) then
-        return require(real_path)
+    local first_char = module_name:sub(1, 1)
+    local real_path
+    if first_char == '/' then
+        --绝对路径处理
+        real_path = module_name
     else
-        return require(module_name)
+        --相对路径转化为绝对路径
+        local deep = 2
+        local tmp_name = debug_getinfo(2, "n")['name']
+        if tmp_name == 'require_once' then
+            deep = 3
+        end
+        local call_path = dirname(debug_getinfo(deep, "S").source:sub(2))
+        real_path = call_path..'/'..module_name
     end
+    if not file_exists(real_path) then
+        error('require file not exist. path: '..real_path)
+    end
+    return dofile(real_path)
+
+end
+-- PHP的require_once方式加载
+function Tools:require_once(module_name)
+    if empty(self._REQUIRE_CACHE[module_name]) then
+        self._REQUIRE_CACHE[module_name] = self:require(module_name)
+    end
+    return self._REQUIRE_CACHE[module_name]
 end
 return Tools

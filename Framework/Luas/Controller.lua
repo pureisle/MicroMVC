95e524e763dc3cda1a158b63b8e65db367fdb4ee
xiaodu <hzyhouzhiyuan@qq.com>
2019-06-24 02:47:50 -0400

    增加模板变量设置方法

--- Framework/Luas/Controller.lua
+++ Framework/Luas/Controller.lua
@@ -5,10 +5,19 @@
 Controller = Class:new('Controller')
 -- 构造方法
 function Controller:new ()
-    return Class:new('Controller', self)
+    local tmp = Class:new('Controller', self)
+    tmp.render_params = {}
+    return tmp
 end
 function Controller:classCheck()
     return self.class
+end
+function Controller:assign(params)
+    self.render_params = params
+    return self
+end
+function Controller:getAssign()
+    return self.render_params
 end
 function Controller:getGetParams()
     return ngx.req.get_uri_args()

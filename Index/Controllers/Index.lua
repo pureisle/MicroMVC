3914948acc603d8b6684c6787c7fd45e868ca468
xiaodu <hzyhouzhiyuan@qq.com>
2019-06-24 02:49:42 -0400

    使用模板输出

--- Index/Controllers/Index.lua
+++ Index/Controllers/Index.lua
@@ -1,7 +1,8 @@
 -- 首先继承Controller基类
 local Index_Controller = Controller:new()
 function Index_Controller:indexAction()
-    ngx.say('Hello,MicroMVC')
+    -- ngx.say('Hello,MicroMVC')
+    self:assign({text = 'Hello,MicroMVC'})
     return true
 end
 return Index_Controller

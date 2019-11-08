314f1b8610345b92a3f07647c302e71540a2bf3f
xiaodu <hzyhouzhiyuan@gmail.com>
2019-11-08 20:59:37 +0800

    添加单元测试

---
+++ Framework/Tests/TestContext.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Context类单元测试
+ *
+ *
+ * @author zhiyuan <zhiyuan12@staff.weibo.com>
+ */
+namespace Framework\Tests;
+use Framework\Libraries\Context;
+use Framework\Libraries\TestSuite;
+
+class TestContext extends TestSuite {
+    const TEST_CLASS_NAME = \Framework\Libraries\Context::class;
+    public function beginTest() {
+        $this->t = new Context();
+    }
+    public function testmSet() {
+        $data = array(
+            'a' => 1,
+            'b' => 2,
+            'c' => 3
+        );
+        $this->t->mSet($data);
+        $this->assertEq($this->t->a, $data['a']);
+        $this->assertEq($this->t->b, $data['b']);
+        $this->assertEq($this->t->c, $data['c']);
+        $tmp        = 5;
+        $this->t->c = $tmp;
+        $this->assertEq($this->t->c, $tmp);
+
+    }
+    public function testmGet() {
+        $ret = $this->t->mGet(array('a', 'b'));
+        $this->assertEq(isset($ret['a']), true);
+    }
+}
